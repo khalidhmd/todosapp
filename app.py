83ebd3cc761c1b99f3f836a42459f331aa2c60cc
khalidhmd <khalidhmd@outlook.com>
2020-09-03 19:01:36 +0200

    add fetch api

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for,jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -24,11 +24,11 @@ def index():
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    description = request.form.get('description')
+    description = request.get_json()['description']
     todo = Todo(description=description)
     db.session.add(todo)
     db.session.commit()
-    return redirect(url_for('index'))
+    return jsonify({'id':todo.id,'description':todo.description})
 
 
 if __name__ == '__main__':
