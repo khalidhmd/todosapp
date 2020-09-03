8f9c59f8e8956a1a735fb0d4d70d34334f498c57
khalidhmd <khalidhmd@outlook.com>
2020-09-03 21:16:35 +0200

    add migrations

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
 from flask import Flask, render_template, redirect, request, url_for,jsonify,abort
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user1:user1@localhost:5432/tododb'
 
 db = SQLAlchemy(app)
 
+migrate = Migrate(app,db)
+
+
 class Todo(db.Model):
   __tablename__ = 'todos'
   id = db.Column(db.Integer, primary_key=True)
   description = db.Column(db.String(), nullable=False)
+  completed = db.Column(db.Boolean(),nullable=False, default=False)
 
   def __repr__(self):
     return f'<Todo ID: {self.id}, description: {self.description}>'
 
-db.create_all()
+# db.create_all() #disable this before flask db migrate
 
 
 @app.route('/')
